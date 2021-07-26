19f2aa4ee90a475332bcaef944228c5f2ded8e84
meck512 <m_eck12@outlook.com>
2021-07-26 19:13:47 -0400

    intern object

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,6 +1,6 @@
 const Employee = require('../lib/Employee.js')
 
-test('creates a empoyee object w/ name, id, & email', () => {
+test('creates a employee object w/ name, id, & email', () => {
 
     const steve = new Employee('Steve', 12345, 'steve@gmail.com');
 
